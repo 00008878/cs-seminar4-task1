296ae287fcdd462e87ceaf6dd7f45d9e6e143d18
Ulugbek <ulugbek.turaqulov311@gmail.com>
2020-10-21 23:35:47 +0500

    Initial commit

---
+++ seminar4_task1.py
@@ -0,0 +1,19 @@
+m = int(input())
+def countdown(n):
+    if n<=0:
+        print("Blastoff")
+    else:
+        print(n)
+        countdown(n-1)
+
+def countup(n):
+    if n>=0:
+        print("Blastoff")
+    else:
+        print(n)
+        countup(n+1)
+
+if m > 0:
+    countdown(m)
+else:
+    countup(m)
